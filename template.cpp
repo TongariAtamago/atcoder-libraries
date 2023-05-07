ce84dbdafd5994366071279d86a09cfe50cea83c
T <takuyamao@outlook.jp>
2023-05-08 01:05:56 +0900

    Create template.cpp

---
+++ template.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep1(a) for (int i = 0; i < a; i++)
+#define rep2(i, a) for (int i = 0; i < a; i++)
+#define rep3(i, a, b) for (int i = a; i < b; i++)
+#define rep4(i, a, b, c) for (int i = a; i < b; i += c)
+#define overload4(a, b, c, d, e, ...) e
+#define rep(...) overload4(__VA_ARGS__, rep4, rep3, rep2, rep1)(__VA_ARGS__)
+using ll = long long;
+using P = pair<int, int>;
+using VI = vector<int>;
+using VL = vector<ll>;
+using VVI = vector<vector<int>>;
+using VVL = vector<vector<ll>>;
+
+int main(){
+  return 0;
+}
