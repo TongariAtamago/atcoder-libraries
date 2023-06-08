99692a0e128f86709cd0349aad782fc743a4bc87
T <takuyamao@outlook.jp>
2023-06-08 09:03:28 +0900

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -1,19 +1,21 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define rep1(a) for (int i = 0; i < a; i++)
-#define rep2(i, a) for (int i = 0; i < a; i++)
-#define rep3(i, a, b) for (int i = a; i < b; i++)
-#define rep4(i, a, b, c) for (int i = a; i < b; i += c)
+#include <atcoder/all>
+using namespace atcoder;
+#define rep1(a)          for(int i = 0; i < a; i++)
+#define rep2(i, a)       for(int i = 0; i < a; i++)
+#define rep3(i, a, b)    for(int i = a; i < b; i++)
+#define rep4(i, a, b, c) for(int i = a; i < b; i += c)
 #define overload4(a, b, c, d, e, ...) e
 #define rep(...) overload4(__VA_ARGS__, rep4, rep3, rep2, rep1)(__VA_ARGS__)
 using ll = long long;
-using P = pair<int, int>;
+using P = pair<int,int>;
 using VI = vector<int>;
 using VL = vector<ll>;
 using VVI = vector<vector<int>>;
 using VVL = vector<vector<ll>>;
 
-int main(){
+int main() {
   ios::sync_with_stdio(false);
   cin.tie(nullptr);
   return 0;
