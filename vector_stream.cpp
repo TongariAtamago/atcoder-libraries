eef60c93d07f578bed4b47ba4f2bfb40ea562eae
T <takuyamao@outlook.jp>
2025-09-09 21:18:56 +0900

    Update vector_stream.cpp

--- vector_stream.cpp
+++ vector_stream.cpp
@@ -1,9 +1,19 @@
-template<class T> ostream& operator<<(ostream& os, const vector<T>& vec) {
-	// os << "[ ";
+// 1次元vectorのオーバーロード
+template<class T>
+std::ostream& operator<<(std::ostream& os, const std::vector<T>& vec) {
     long long int n = vec.size();
-	for ( long long int i = 0; i < n; i++ )
-        os << vec[i] << (i==n-1?"":" ");
-	// os << "]"; 
+    for (long long int i = 0; i < n; ++i) {
+        os << vec[i] << (i == n - 1 ? "" : "");
+    }
+    return os;
+}
+// 2次元vectorのオーバーロード
+template<class T>
+std::ostream& operator<<(std::ostream& os, const std::vector<std::vector<T>>& vec) {
+    long long int n = vec.size();
+    for (long long int i = 0; i < n; ++i) {
+        os << vec[i] << (i == n - 1 ? "" : "\n");
+    }
     return os;
 }
 template<class T> istream& operator>>(istream& is, vector<T>& vec) {
