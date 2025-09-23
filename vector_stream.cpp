b7fb5660565d5004e0adec21fad1d41e19e4c6a2
T <takuyamao@outlook.jp>
2025-09-23 12:49:57 +0900

    Add input and output overloads for vectors

--- vector_stream.cpp
+++ vector_stream.cpp
@@ -1,4 +1,4 @@
-// 1次元vectorのオーバーロード
+// 1次元vectorの出力
 template<class T>
 std::ostream& operator<<(std::ostream& os, const std::vector<T>& vec) {
     long long int n = vec.size();
@@ -7,7 +7,7 @@ std::ostream& operator<<(std::ostream& os, const std::vector<T>& vec) {
     }
     return os;
 }
-// 2次元vectorのオーバーロード
+// 2次元vectorの出力
 template<class T>
 std::ostream& operator<<(std::ostream& os, const std::vector<std::vector<T>>& vec) {
     long long int n = vec.size();
@@ -16,8 +16,19 @@ std::ostream& operator<<(std::ostream& os, const std::vector<std::vector<T>>& ve
     }
     return os;
 }
-template<class T> istream& operator>>(istream& is, vector<T>& vec) {
-	for ( T& item : vec )
-		is >> item;
-	return is;
+// 1次元vectorの入力
+template<class T>
+std::istream& operator>>(std::istream& is, std::vector<T>& vec) {
+    for (T& item : vec) {
+        is >> item;
+    }
+    return is;
+}
+// 2次元vectorの入力
+template<class T>
+std::istream& operator>>(std::istream& is, std::vector<std::vector<T>>& vec) {
+    for (auto& row : vec) {
+        is >> row;
+    }
+    return is;
 }
