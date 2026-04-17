15ca7a84c2f17c8249ec7df8247e01d1ba331494
T <takuyamao@outlook.jp>
2026-04-17 19:21:38 +0900

    Refactor RLE function to use templates

--- RLE.cpp
+++ RLE.cpp
@@ -1,17 +1,23 @@
 //Run-Length-Encoding (ランレングス圧縮)
-void RLE(string &x, vector<pair<char, int>> &tango) {
-  char now = x[0];
-  int cnt = 0;
-  for (auto c : x) {
-    if (now == c) {
-      cnt++;
-      continue;
-    } else {
-      tango.emplace_back(now, cnt);
-      now = c;
-      cnt = 1;
+template <class container>
+auto RLE(const container &x) {
+    using T = typename container::value_type;
+    vector<pair<T, int>> res;
+    if (x.empty()) return res;
+
+    T now = x[0];
+    int cnt = 0;
+
+    for (const auto &c : x) {
+        if (c == now) {
+            cnt++;
+        } else {
+            res.emplace_back(now, cnt);
+            now = c;
+            cnt = 1;
+        }
     }
-  }
-  tango.emplace_back(now, cnt);
-  return;
+    res.emplace_back(now, cnt);
+
+    return res;
 }
